f477180916ffd6b8630af380eb4c31f26e764312
laloix_t <tanguy.laloix@epitech.eu>
2017-02-16 01:16:21 +0100

    Fake news list

--- system/news.php
+++ system/news.php
@@ -1,4 +1,4 @@
-<?
+<?php
   $i = 0;
   while ($i < 3) {
     $content = file_get_contents('http://loripsum.net/api/1/');
@@ -7,19 +7,19 @@
 <div class="panel panel-default">
   <div class="panel-heading">
     <h4 class="panel-title">
-      <b><a data-toggle="collapse" href="#collapse<? echo $i; ?>" style="cursor: pointer; text-decoration: none;">New Title #<? echo $i + 1; ?> !</a></b>
+      <b><a data-toggle="collapse" href="#collapse<?php echo $i; ?>" style="cursor: pointer; text-decoration: none;">New Title #<?php echo $i + 1; ?> !</a></b>
     </h4>
   </div>
-  <div id="collapse<? echo $i; ?>" class="panel-collapse collapse in">
+  <div id="collapse<?php echo $i; ?>" class="panel-collapse collapse in">
     <div class="panel-body">
-      <? echo $content; ?>
+      <?php echo $content; ?>
       <a style="float:right; cursor: pointer; text-decoration: none;">Read more...</a>
     </div>
     <div class="panel-footer">01-10-2017</div>
   </div>
 </div>
 
-<?
+<?php
   $i += 1;
   }
 ?>
